47518e45490dd52b77af0beced25283428e40931
SmthOnYourMind <mastaden46@gmail.com>
2024-03-27 14:53:55 +0500

    Добавлены команды контекстного меню, и др

---
+++ Ribbon.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Interop.Word;
+
+namespace word_test
+{
+    [ComVisible(true)]
+    public class Ribbon : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+
+        public Ribbon()
+        {
+        }
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+            return GetResourceText("word_test.Ribbon.xml");
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks
+        //Create callback methods here. For more information about adding callback methods, visit https://go.microsoft.com/fwlink/?LinkID=271226
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+        public void GetButtonID(Office.IRibbonControl control)
+        {
+            Microsoft.Office.Interop.Word.Range currentRange = Globals.ThisAddIn.Application.Selection.Range;
+
+            string current_selected = currentRange.Text;
+
+            if (control.Id == "context_button1")
+            {
+                currentRange.Text = "Переписанный текст (Было: " + current_selected + ")";
+            }
+            else if (control.Id == "context_button2")
+            {
+                currentRange.Text = "Дополненный текст (Было: " + current_selected + ")";
+            }
+
+        }
+
+        public void GetToken(Office.IRibbonControl control)
+        {
+            MessageBox.Show("Введите токен");
+        }
+
+        public void GetRequest(Office.IRibbonControl control)
+        {
+            MessageBox.Show("Введите запрос");
+        }
+
+        public void SendPostRequest()
+        {
+            /*
+            var options = new RestClientOptions("")
+            {
+                MaxTimeout = -1,
+            };
+            var client = new RestClient(options);
+            var request = new RestRequest("https://ngw.devices.sberbank.ru:9443/api/v2/oauth", Method.Post);
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddHeader("Accept", "application/json");
+            request.AddParameter("scope", "GIGACHAT_API_PERS");
+            RestResponse response = await client.ExecuteAsync(request);
+            Console.WriteLine(response.Content);
+            */
+        }
+
+
+        #endregion
+
+        #region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
